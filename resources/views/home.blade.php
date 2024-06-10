5960edcde11c8fd3300ba641f7f8f7027600bf8e
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-10 17:45:17 +0200

    fix: card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,19 @@
 @section('content')
     <h1 class="text-center">Film</h1>
     <div class="container">
-        <div class="row-col-3">
-            <div class="col-3">
-                @foreach ($movies as $movie)
-                    <div class="card">
-                        <img src="{{$movie->image}}" alt="{{$movie->title}}">
+        <div class="row g-3">
+            @foreach ($movies as $movie)
+                <div class="col-3">
+                    <div class="card text-white">
+                        <img src="{{ $movie->image }}" alt="{{ $movie->title }}">
                         <div class="card-body">
-                            <h4>Titolo:{{$movie->title}}</h4>
-                            <h5>Titolo originale:{{$movie->original_title}}</h5>
-                            <p>Data di uscita:{{$movie->date}}</p>
+                            <h4>Titolo:{{ $movie->title }}</h4>
+                            <h5>Titolo originale:{{ $movie->original_title }}</h5>
+                            <p>Data di uscita:{{ $movie->date }}</p>
                         </div>
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection

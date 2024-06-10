7229ae3d072063da374783a0d115cb50286d88b7
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-10 17:24:12 +0200

    feat: card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,20 @@
 
 @section('content')
     <h1 class="text-center">Film</h1>
-    <ul>
-        @foreach ($movies as $movie)
-            <li>{{ $movie->title }}</li>
-        @endforeach
-    </ul>
+    <div class="container">
+        <div class="row-col-3">
+            <div class="col-3">
+                @foreach ($movies as $movie)
+                    <div class="card">
+                        <img src="{{$movie->image}}" alt="{{$movie->title}}">
+                        <div class="card-body">
+                            <h4>Titolo:{{$movie->title}}</h4>
+                            <h5>Titolo originale:{{$movie->original_title}}</h5>
+                            <p>Data di uscita:{{$movie->date}}</p>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 @endsection
